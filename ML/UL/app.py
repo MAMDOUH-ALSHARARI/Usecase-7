d342822ef7f1f888b137700861b8676092aed6af
Mamdoun Alsharari <mamdouh.h.alsharari@gmail.com>
2024-11-16 02:00:37 +1100

    deploy DBSCAN model

--- ML/UL/app.py
+++ ML/UL/app.py
@@ -14,8 +14,7 @@ award = st.number_input("Player award : ", step=1)
 # Prediction button
 if st.button("Predict Category"):
         # API request URL
-        url = "https://football-players-transfer.onrender.com/DBSCAN_Football_Players/predict"
-        
+        url = "https://usecase-7-2-5npk.onrender.com/DBSCAN_Football_Players/predict"
         # Data for the POST request
         data = {
 
