75ff4acd09b3b6322c5d4308bd263ed099cfc488
Mamdoun Alsharari <mamdouh.h.alsharari@gmail.com>
2024-11-16 03:27:30 +1100

    deploy DBSCAN model

--- ML/UL/main.py
+++ ML/UL/main.py
@@ -29,11 +29,12 @@ def preprocessing(input_features: InputFeatures):
         'current_value':input_features.current_value,
         'award': input_features.award
     }
+    new_data = pd.DataFrame([dict_f])
     # Get the feature names the scaler was trained on
-    feature_names = DBSCAN_scaler.get_feature_names_out() 
+    #feature_names = DBSCAN_scaler.get_feature_names_out() 
 
     # Ensure new_data has the same columns and order as the original data
-    new_data = dict_f.reindex(columns=feature_names) # Reorder or add missing columns
+    #new_data = dict_f.reindex(columns=feature_names) # Reorder or add missing columns
 
     # 3. Scale the new data using the loaded scaler
     new_data_scaled = DBSCAN_scaler.transform(new_data)
@@ -58,8 +59,12 @@ def read_root():
 
 @app.post("/DBSCAN_Football_Players/predict")
 async def Football_Players(input_features: InputFeatures):
+    #data = preprocessing(input_features)
+    #print('done')
+    #y_pred = DBSCAN_model.predict(data)
+    #print('done')
+    #return {"pred": y_pred.tolist()[0]}
+
     data = preprocessing(input_features)
-    print('done')
-    y_pred = DBSCAN_model.predict(data)
-    print('done')
-    return {"pred": y_pred.tolist()[0]}
+    y_pred = DBSCAN_model.fit_predict(data)  # Use fit_predict instead of predict
+    return {"pred": int(y_pred[0])}
